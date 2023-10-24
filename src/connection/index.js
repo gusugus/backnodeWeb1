2a2ab99321e43f64e15b5320a2062b7a46f4b73d
gusagus <gang_emelec@yahoo.com>
2023-10-24 12:19:40 -0500

    c4

--- src/connection/index.js
+++ src/connection/index.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2/promise");
 const connection = mysql.createPool({
-    host: 'localhost',
+    host: 'db4free.net',
     port: 3306,
-    database: "backnode",
-    user: "root",
-    password: "",
+    database: "gang_emelec",
+    user: "gang_emelec",
+    password: "gang_emelec",
 });
 module.exports = connection
